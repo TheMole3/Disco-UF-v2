52e2b4870a6d9711a8816a0a9d32ba784973e439
Hugo Arnlund <hugomelo04@gmail.com>
2023-01-11 09:55:15 +0100

    Update socket.js

--- admin/src/lib/socket.js
+++ admin/src/lib/socket.js
@@ -11,8 +11,11 @@ export const socket = socketClient;
 socket.on("connect_error", (err) => {
     // If user is not logged in, redirect
     console.log(err); // prints the message associated with the error
-    if(err.data == 401 || err.data == 403) {
-        console.log("Redirect")
-        //if(global.location) global.location.replace("https://apidisco.melo.se/login")
+    if(err.data == 401) {
+        if(global.location) global.location.replace("https://apidisco.melo.se/login")
+    }
+
+    if( err.data == 403) {
+        alert("Du har inte behörighet")
     }
 });
