bac9e4ad7312f7e108bc990109c6a5ed1d2ef933
Hugo Arnlund <hugomelo04@gmail.com>
2023-01-11 09:40:00 +0100

    build

--- admin/src/lib/socket.js
+++ admin/src/lib/socket.js
@@ -12,6 +12,7 @@ socket.on("connect_error", (err) => {
     // If user is not logged in, redirect
     console.log(err); // prints the message associated with the error
     if(err.data == 401 || err.data == 403) {
-        if(global.location) global.location.replace("https://apidisco.melo.se/login")
+        console.log("Redirect")
+        //if(global.location) global.location.replace("https://apidisco.melo.se/login")
     }
 });
