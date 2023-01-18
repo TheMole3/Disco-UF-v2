ec80d194b93b2d14f24c4e285b001fd08826547b
Hugo Arnlund <hugomelo04@gmail.com>
2023-01-18 09:34:38 +0100

    Touch user fix

---
+++ client/build/_app/immutable/chunks/2-74d1f91c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7d13bc8a.js";export{t as component};
--- client/build/_app/immutable/chunks/2-80af93b2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-1b687aed.js";export{t as component};
